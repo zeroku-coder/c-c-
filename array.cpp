cb419b36cfb4a858641e278833d4be4ee0ccd69d
zeroku-coder <73018643+zeroku-coder@users.noreply.github.com>
2021-03-01 14:53:53 +0530

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -1,3 +1,4 @@
+//program to use array in c++
 #include<iostream>
 #include<conio.h>
 using namespace std;
@@ -5,15 +6,15 @@ int main()
 {   int n,i,a[20];
     
 
-    cout<<"Enter the length of array:";
+    cout<<"Enter the length of array:";//define the size of array
     cin>>n;
     cout<<"enter the element of array:";
-    for(i=0;i<n;i++)
+    for(i=0;i<n;i++)//loop to enter the elements in array
     {
         cin>>a[i];
     }
     cout<<"the element of the array are:";
-    for(i=0;i<n;i++)
+    for(i=0;i<n;i++)//loop to print element of array
     {
         cout<<a[i]<<" ";
     }
